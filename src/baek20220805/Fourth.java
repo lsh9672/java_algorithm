9d0af24fa9e43d811f3bab2e93df7c47348b416e
leesh <lsh80165@gmail.com>
2022-08-05 23:44:15 +0900

    baekjoon_2022_08_05

---
+++ src/baek20220805/Fourth.java
@@ -0,0 +1,52 @@
+package baek20220805;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Fourth {
+
+    static int N;
+
+    // 해당범위까지 소수 다 구해두기
+    static boolean findPrime(int num) {
+
+        //1은 소수가 아님.
+        if(num == 1) return false;
+
+        //에라토스테네스의 체를 이용해서 소수를 구함.
+        int m= (int)Math.sqrt(num);
+
+        for(int i = 2; i <= m; i++) {
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+
+    static void recursive(String str) {
+
+        if(str.length() == N) {
+            System.out.println(Integer.parseInt(str));
+            return;
+        }
+
+        for(int i = 1; i <= 9; i++) {
+
+            // 숫자 한개씩 이어 붙이고 소수인지 판별 - 소수이면 재귀호출
+            if(findPrime(Integer.parseInt(str+i)))
+                recursive(str+i);
+        }
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        recursive("");
+
+    }
+
+}
