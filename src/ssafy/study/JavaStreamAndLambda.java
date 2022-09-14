7875db71cb16322587ed9608b83d7a0316df8c6c
leesh <lsh80165@gmail.com>
2022-09-14 20:05:02 +0900

    baekjoon_2022_09_14

---
+++ src/ssafy/study/JavaStreamAndLambda.java
@@ -0,0 +1,26 @@
+package ssafy.study;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class JavaStreamAndLambda {
+
+    public static void main(String[] args) {
+
+        int[] aaa1 = {1,2,3,4,5};
+
+//        List<Integer> aaa2 =
+//
+//        long count = Arrays.stream(aaa).filter((i) -> i % 2 == 0).sum();
+//        System.out.println(count);
+//
+//        IntStream ttt = IntStream.range(1,10);
+//        System.out.println(ttt);
+
+
+
+    }
+}
