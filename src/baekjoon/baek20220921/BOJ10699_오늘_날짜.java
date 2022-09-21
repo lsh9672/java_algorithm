4c21c2cd2ad2c841e224f5db85230c6ece878f6d
leesh <lsh80165@gmail.com>
2022-09-21 23:50:11 +0900

    baekjoon_2022_09_21

---
+++ src/baekjoon/baek20220921/BOJ10699_오늘_날짜.java
@@ -0,0 +1,11 @@
+package baekjoon.baek20220921;
+
+import java.time.LocalDate;
+
+public class BOJ10699_오늘_날짜 {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
